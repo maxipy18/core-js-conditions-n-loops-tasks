0fb619e18d9cd830c9e3c3e01ca04dcfe378210b
Maxim Krestyaninov <maxipy@ya.ru>
2025-01-08 23:05:25 +0400

    feat: add isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -356,8 +356,11 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  for (let i = 0; i < String(num).length; i += 1) {
+    if (String(num)[i] === String(digit)) return true;
+  }
+  return false;
 }
 
 /**
